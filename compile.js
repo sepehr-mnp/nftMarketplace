775eb5b8f03caf11a81700c77eab5a13af3535f0
sepehrmnp <sepehr.mirnasrollahi@aut.ac.ir>
2023-03-07 23:59:48 -0800

    Create compile.js

---
+++ compile.js
@@ -0,0 +1,27 @@
+ const path = require('path');
+ const fs = require('fs-extra');
+ const solc= require('solc');
+ const { url } = require('inspector');
+
+ const buildPath = path.resolve(__dirname, 'build');
+
+ fs.removeSync(buildPath);
+
+ const campaignPath= path.resolve(__dirname,'contract',   'nftMarketplace.sol');
+ const source = fs.readFileSync(campaignPath,'utf-8');
+ var input = {
+    language: 'Solidity',
+    sources: {
+             'ERC20.sol': {
+              content: source
+   }
+},
+    settings: {
+              outputSelection: {
+              '*': {
+                 '*': ['*']
+                  }
+               }
+      }
+};
+module.exports = JSON.parse(solc.compile(JSON.stringify(input)))['contracts']['nftMarketplace.sol']['NFTMarketplace'];
